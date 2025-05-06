e9e1f12e853fe2d922bf6e7b86604c2d9ee1110c
Thiago Barbosa <thiago@acamp.com>
2025-05-06 03:19:46 +0200

    improve list prefixes

--- markify/lib/markdown/handlers/lists.ts
+++ markify/lib/markdown/handlers/lists.ts
@@ -8,18 +8,21 @@ export const processList = async (
   url?: string | null
 ): Promise<string> => {
   const parts: string[] = []
-  const children = $list.children().toArray()
 
-  for (const li of children) {
+  // Process each child of the list
+  for (const li of $list) {
     const $li = $(li)
 
     const itemText = await processChildren($, $li, context, url)
 
     if (itemText.trim().length) {
+      // Add prefix only if there's no image or heading inside the list item
+      const addPrefix = !$li.find('img').length && !$li.find('h1, h2, h3, h4, h5, h6').length
+
       if (context === 'table') {
-        parts.push(`<li>${itemText.trim()}</li>`)
+        parts.push('<br><li>' + (addPrefix ? '* ' : '') + itemText.trim() + '</li>')
       } else {
-        parts.push(`\n* ${itemText.trim()}`)
+        parts.push('\n' + (addPrefix ? '* ' : '') + itemText.trim())
       }
     }
   }
